dc1c8433106558d7b75cfbb968385ea93cc65baa
QUORIX\quori <kaiserdoragon1121@gmail.com>
2025-09-29 21:38:01 +0900

    PCのTOPはおおむね完了

--- theme/footer.php
+++ theme/footer.php
@@ -1,32 +1,25 @@
 <footer class="footer">
-  <div class="footer_top">
-    <div class="address--wrap">
-      <h2 class="address--ttl">
-        <?php bloginfo('name'); //サイトタイトル
-        ?>
-      </h2>
-      <address class="address">住所<br>電話</address>
-    </div>
-    <div class="sitemap">
-      <ul class="sitemap--list">
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-      </ul>
-      <ul class="sitemap--list">
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-        <li class="sitemap--item">ページ名</li>
-      </ul>
-    </div>
-  </div>
-  <div class="footer_bottom">
-    <p class="footer_bottom--copy">
-      <small><span lang="en">&copy;</span>20xx <?php bloginfo('name'); ?></small>
-    </p>
-  </div>
+    <a href="#" class="footer--logo">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/common/footer_logo.svg" alt="株式会社河合商店" width="253" height="61">
+    </a>
+    <ul>
+        <li>
+            <a href="<?php echo esc_url(home_url('/')); ?>"><span>HOME</span>ホーム</a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/about')); ?>"><span>About Us</span>河合商店の取り組み</a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/recruit')); ?>"><span>Recruit</span>採用情報</a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/company')); ?>"><span>Company</span>企業紹介</a>
+        </li>
+        <li>
+            <a href="<?php echo esc_url(home_url('/news')); ?>"><span>News</span>お知らせ</a>
+        </li>
+    </ul>
+    <p><small>http//yexttexttttexxxt.@text</small></p>
 </footer>
 </div>
 <?php wp_footer(); ?>
