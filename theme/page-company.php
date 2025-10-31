ff59933c02af9bbd4c84c3022884d24b6925ab33
QUORIX\quori <kaiserdoragon1121@gmail.com>
2025-10-31 20:33:31 +0900

    お問い合わせのページ以外のPC～SPおおむね作成

--- theme/page-company.php
+++ theme/page-company.php
@@ -81,12 +81,68 @@
         <p>w</p>
       </div>
     </h2>
-    <div class="container">
+    <div class="company_overview--info">
+      <dl>
+        <div>
+          <dt>会社</dt>
+          <dd>株式会社河合商店</dd>
+        </div>
+        <div>
+          <dt>代表</dt>
+          <dd>代表取締役 河合祐樹</dd>
+        </div>
+        <div>
+          <dt>TEL</dt>
+          <dd>0568-48-0600</dd>
+        </div>
+        <div>
+          <dt>所在地</dt>
+          <dd>
+            〒480-0202<br>
+            愛知県西春日井郡豊山町<br>
+            豊場字幸田162番地</dd>
+        </div>
+        <div>
+          <dt>FAX</dt>
+          <dd>0568-48-0601</dd>
+        </div>
+        <div>
+          <dt>資本金</dt>
+          <dd>500万</dd>
+        </div>
+        <div>
+          <dt>従業員数</dt>
+          <dd>
+            12名<br>
+            運行管理者／1名　<br class="is-hidden_pc">中部愛貨物第34664号<br>
+            整備管理者／2名　<br class="is-hidden_pc">中部愛整管第6915号、<br class="is-hidden_pc">6824号<br>
+          </dd>
+        </div>
+        <div>
+          <dt>事業内容</dt>
+          <dd>
+            一般貨物自動車運送始業／<br class="is-hidden_pc">中部自貨第245号　<br class="is-hidden_pc">事業者番号：560025605<br>
+            第一種貨物利用運送事業／<br class="is-hidden_pc">中運自貨第272号　<br class="is-hidden_pc">事業者番号：0504030
+          </dd>
+        </div>
+      </dl>
+    </div>
+    <div class="container company_overview--history">
       <h3>沿革</h3>
+      <dl>
+        <dt>令和2年2月</dt>
+        <dd>運送会社（利用運送）河合商会を創立</dd>
+        <dt>令和5年2月</dt>
+        <dd>河合商会から株式会社河合商店へ法人化</dd>
+        <dt>令和7年5月</dt>
+        <dd>名古屋市北区から西春日井郡豊山町幸田へ本社移転</dd>
+        <dt>令和7年8月</dt>
+        <dd>春日井市に営業所（車両塗装・板金修理・仮装仲介施工）を開設</dd>
+      </dl>
     </div>
     <div class="company_overview--gmap">
       <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1629.193707860087!2d136.90513165059318!3d35.24661646610286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60037300604005bd%3A0xf7ba0f838ecbbcab!2z5qCq5byP5Lya56S-5rKz5ZCI5ZWG5bqX!5e0!3m2!1sja!2sjp!4v1761519533843!5m2!1sja!2sjp" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1629.193707860087!2d136.90513165059318!3d35.24661646610286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f20!3m3!1m2!1s0x60037300604005bd%3A0xf7ba0f838ecbbcab!2z5qCq5byP5Lya56S-5rKz5ZCI5ZWG5bqX!5e0!3m2!1sja!2sjp!4v1761519533843!5m2!1sja!2sjp" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   </section>
